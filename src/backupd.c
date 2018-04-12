4c4dea89c9728b67298e4042b2c69ab2c57c600a
Brian Clark <bclarkx2@gmail.com>
2018-04-12 11:33:08 -0400

    Add steps to be filled in for daemonization

--- src/backupd.c
+++ src/backupd.c
@@ -1,5 +1,5 @@
 
-/*
+/***************************************************
  * backupd.c
  * Author: Brian Clark
  * Date: 4/12/18
@@ -10,7 +10,26 @@
 
 int main(int arc, char* argv[]){
 
-    printf("Hello World!\n");
+
+    // Steps
+
+    // Fork parent process
+
+    // Terminate parent iff forking was successful
+
+    // Use setsid to detach from calling TTY
+
+    // Catch signals
+
+    // Fork again and let parent terminate
+
+    // Change daemon's working directory 
+ 
+    // Use umask to acquire appropriate file permissionss
+
+    // Close any inherited file descriptors
+
+    printf("I AM DAEMON\n");
 
     return 0;
 }
