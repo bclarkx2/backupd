38f82763e2af02fb001eaec720eb945bf9071e8f
Brian Clark <bclarkx2@gmail.com>
2018-04-17 16:48:35 -0400

    Implement forking from parent

--- src/backupd.c
+++ src/backupd.c
@@ -6,7 +6,16 @@
  * Description: Linux filesystem monitoring daemon
  */
 
-#include <stdio.h>
+#include <stdio.h>              // I/O
+#include <stdlib.h>             // exit
+#include <sys/types.h>          // pid_t
+#include <unistd.h>             // fork
+
+// Error codes
+#define SUCCESS 0
+#define FAILURE 1
+#define FORK_FAILURE 2
+
 
 int main(int arc, char* argv[]){
 
@@ -14,6 +23,20 @@ int main(int arc, char* argv[]){
     // Steps
 
     // Fork parent process
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid < 0){
+        fprintf(stderr, "Fork unsuccessful\n");
+        exit(FORK_FAILURE);
+    }
+    else if (pid > 0){
+        printf("Fork successful: killing parent\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    printf("Fork successful: continuing with child\n");
 
     // Terminate parent iff forking was successful
 
